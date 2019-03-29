e38a953a6ada5af1680ff6ca0c6067e2f7a37f2c
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-03-29 00:23:35 -0300

    count usando async, await e try/catch

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -5,10 +5,13 @@ const BillingCycle = restful.model('BillingCycle', BillingCycleSchema)
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
-BillingCycle.route('count', (req, res, next) => {
-    BillingCycle.count().exec()
-        .then(value => res.json({ value }))
-        .catch(error => res.status(500).json({ errors: [error] }))
+BillingCycle.route('count', async (req, res, next) => {
+    try {
+        const value = await BillingCycle.count().exec()
+        res.json({ value })
+    } catch (error) {
+        res.status(500).json({ errors: [error] })
+    }
 })
 
 export default BillingCycle
