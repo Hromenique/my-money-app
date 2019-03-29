e275b8c7f07e226c044dfd53135b121b3192b7ef
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-03-28 23:59:33 -0300

    count endpoint

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -5,6 +5,15 @@ const BillingCycle= restful.model('BillingCycle', BillingCycleSchema)
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error){
+            res.status(500).json({errors: [error]})
+        }else{
+            res.json({value})
+        }
+    })
+})
 
 export default BillingCycle
 
