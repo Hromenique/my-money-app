36c51105963602bf58a123b221abe2497e254e6d
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-03-29 00:08:13 -0300

    count com promise

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,18 +1,14 @@
 import BillingCycleSchema from './BillingCycleSchema'
 import restful from 'node-restful'
 
-const BillingCycle= restful.model('BillingCycle', BillingCycleSchema)
+const BillingCycle = restful.model('BillingCycle', BillingCycleSchema)
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.updateOptions({ new: true, runValidators: true })
 BillingCycle.route('count', (req, res, next) => {
-    BillingCycle.count((error, value) => {
-        if(error){
-            res.status(500).json({errors: [error]})
-        }else{
-            res.json({value})
-        }
-    })
+    BillingCycle.count().exec()
+        .then(value => res.json({ value }))
+        .catch(error => res.status(500).json({ errors: [error] }))
 })
 
 export default BillingCycle
