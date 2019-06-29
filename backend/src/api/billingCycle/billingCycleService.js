31060633fee06bbc39455eca4cd1152fe86cf78b
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-06-29 00:16:36 -0300

    fim do backend

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,10 +1,13 @@
 import BillingCycleSchema from './BillingCycleSchema'
 import restful from 'node-restful'
+import errorHandler from '../commons/errorHandler'
 
 const BillingCycle = restful.model('BillingCycle', BillingCycleSchema)
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
+
 BillingCycle.route('count', async (req, res, next) => {
     try {
         const value = await BillingCycle.count().exec()
@@ -12,6 +15,22 @@ BillingCycle.route('count', async (req, res, next) => {
     } catch (error) {
         res.status(500).json({ errors: [error] })
     }
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate({
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+    }, {
+            $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+        }, {
+            $project: { _id: 0, credit: 1, debt: 1 }
+        }, (error, result) => {
+            if (error) {
+                res.status(500).json({ errors: [error] })
+            } else {
+                res.json(result[0] || { credit: 0, debt: 0 })
+            }
+        })
 })
 
 export default BillingCycle
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,10 +1,16 @@
 import bodyParse from 'body-parser'
 import express from 'express'
+import allowCors from './cors'
+import queryParser from 'express-query-int'
 
 const port = 3003
 const server = express()
 
 server.use(bodyParse.urlencoded({ extended: true }))
+server.use(bodyParse.json())
+server.use(allowCors)
+server.use(queryParser())
+
 server.listen(port, () => console.log(`BACKEND is running on port ${port}!`))
 
 export default server
