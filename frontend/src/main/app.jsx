9a81fdf7e8f23b5f3d08dfabf5651647c0b78acb
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-07-14 19:13:59 -0300

    rotas

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,10 +3,17 @@ import React from 'react'
 
 import Header from '../commons/template/header'
 import Sidebar from '../commons/template/sidebar'
+import Footer from '../commons/template/footer'
+
+import Routes from './routes'
 
 export default props => (
     <div className='wrapper'>
         <Header />
         <Sidebar />
+        <div className="content-wrapper">
+            <Routes />
+        </div>
+        <Footer />
     </div>
 )
