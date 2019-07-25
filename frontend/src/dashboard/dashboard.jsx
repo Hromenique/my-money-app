5f0b3b29fa7e680f2bc3fd8561871504c2610a1d
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-07-24 22:42:14 -0300

    componentes dashboard e valuebox

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
+
 import ContentHeader from '../commons/template/contentHeader'
 import Content from '../commons/template/content'
+import ValueBox from '../commons/widget/valueBox'
+import Row from '../commons/layout/row'
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <ContentHeader title='Dashboard' small='Versão 1.0'/>
+                <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank'
+                            value='R$ 10' text='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card'
+                            value='R$ 10' text='Total de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money'
+                            value='R$ 10' text='Valor Consolidado' />
+                    </Row>
                 </Content>
             </div>
         )
