41fc8e7668d33d6d1debf80bbb6ee95083813bcb
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-08-02 22:53:39 -0300

    adding tabs

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ContentHeader from '../commons/template/contentHeader'
+import Content from '../commons/template/content'
+import Tabs from '../commons/tab/tabs'
+import TabsHeader from '../commons/tab/tabsHeader'
+import TabsContent from '../commons/tab/tabsContent'
+import TabHeader from '../commons/tab/tabHeader'
 
-export default props => (
-    <div>
-        <h1>Ciclo de Pagamentos</h1>
-    </div>
-)
+class BillingCycle extends Component {
+
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamento' small='Cadastro' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList'/>
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate'/>
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate'/>
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete'/>
+                        </TabsHeader>
+                        <TabsContent>
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycle
