60d8f45cc5149c8b8d0dfc4b9ffd95bf2028cde7
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-09-17 21:18:08 -0300

    add auth form and navbar

--- frontend/src/auth/authReducer.js
+++ frontend/src/auth/authReducer.js
@@ -1,6 +1,6 @@
 const userKey = '_mymoney_user'
 const INITIAL_STATE = {
-    user: JSON.parse(localStorage.getItem(userKey)),
+    user: {name: 'Test', email: 'test@gmail.com'}, //JSON.parse(localStorage.getItem(userKey)),
     validToken: false
 }
 
